02f1cbe72b0bce59cb55dd3893d2d5bc9370272c
Bojan Stojkovski <stojkovskib12@gmail.com>
2020-10-23 00:57:31 +0200

    initial commit

---
+++ CoDesign/js/shared.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    var modal = document.getElementById('id01');
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+});
